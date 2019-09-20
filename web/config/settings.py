3bba32b29a694d90af5fc75b4eb55cff19ea5e3d
Elinaldo Monteiro <elinaldo.java@gmail.com>
2019-09-19 21:33:31 -0300

    Enable STATIC_ROOT if debug False

--- web/config/settings.py
+++ web/config/settings.py
@@ -109,5 +109,5 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# if not DEBUG:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
